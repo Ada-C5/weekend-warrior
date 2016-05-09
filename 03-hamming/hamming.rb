7dc49812ea3609a706e6820b9c482fddfb6e36b8
Jillian Boshart <Jilliliz@gmail.com>
2016-05-09 15:04:18 -0700

    Pulled out comments because they had stopped being relevant.

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -5,11 +5,9 @@ class Hamming
     return distance if x.length == 0
 
     if x[0] == y[0]
-      # return distance if xdif.length == 0
       x = x[1..-1]
       y = y[1..-1]
       Hamming.compute(x, y, distance)
-    #if X = y distance is zero
     else
       distance = distance + 1
       x = x[1..-1]
